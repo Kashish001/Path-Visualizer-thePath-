7fd281dda07a84e2bd6d8806d1275bf16e7ec581
Kashish001 <kashish.gilhotra.001@gmail.com>
2020-10-28 10:42:17 +0530

    Walls Generations, JS/Jquery Work, mouse events

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,15 @@
 
 var totalRows = 40;
 var totalCols = 62;
+var speed = "Fast";
+var algorithmChoosed;
+var createWalls = false;
+var startCell = [0, 61];
+var endCell = [39, 0];
+var inProgress = false;
+var finished = false;
+
+
 
 function createMaze(rows, cols) {
     var maze = "<table>";
@@ -17,3 +26,84 @@ function createMaze(rows, cols) {
 
 var myMaze = createMaze(totalRows, totalCols);
 $("#tableContainer").append(myMaze);
+
+//Nav Bar Backend
+
+$("#mazes .dropdown-item").click(function() {
+
+});
+
+$("#speed .dropdown-item").click(function() {
+    speed = $(this).text();
+    updateDisplayedSpeed();
+});
+
+$("#algorithm .dropdown-item").click(function() {
+    algorithmChoosed = $(this).text();
+    updateAlgo();
+});
+
+$("td").click(function() {
+    var index = $("td").index(this);
+    var startIndex = (startCell[0] * (totalCols)) + startCell[1];
+    var endIndex = (endCell[0] * (totalCols)) + endCell[1];
+    if(!inProgress && !(index == startIndex) && !(index == endIndex)) {
+        if(finished) {
+            clearMaze(keepWalls = true);
+            finished = false;
+        }
+        $(this).toggleClass("wall");
+    }
+});
+
+$("#clearBtn").click(function() {
+    if (inProgress){ return; }
+	clearMaze(keepWalls = false);
+});
+
+$("td").mouseup(function() {
+
+});
+
+
+
+//Functions
+
+function updateDisplayedSpeed() {
+    if(speed == "Fast") $(".displayedSpeed").text("Visualization Speed: Fast");
+    else if(speed == "Slow") $(".displayedSpeed").text("Visualization Speed: Slow");
+    else if(speed == "Normal") $(".displayedSpeed").text("Visualization Speed: Normal");
+    return ;
+}
+
+
+function updateAlgo() {
+    if(algorithmChoosed == "BFS (Breadth First Search)") $(".algo").text("Algorithm: BFS (Breadth First Search)");
+    else if(algorithmChoosed == "DFS (Depth First Search)") $(".algo").text("Algorithm: DFS (Depth First Search)");
+    return ;
+}
+
+function clearMaze( keepWalls ){
+	var cells = $("#tableContainer").find("td");
+	var startCellIndex = (startCell[0] * (totalCols)) + startCell[1];
+	var endCellIndex = (endCell[0] * (totalCols)) + endCell[1];
+	for (var i = 0; i < cells.length; i++){
+			isWall = $( cells[i] ).hasClass("wall");
+			$( cells[i] ).removeClass();
+			if (i == startCellIndex){
+				$(cells[i]).addClass("start"); 
+			} else if (i == endCellIndex){
+				$(cells[i]).addClass("end"); 
+			} else if ( keepWalls && isWall ){ 
+				$(cells[i]).addClass("wall"); 
+			}
+	}
+}
+
+
+//Mouse Events
+
+
+
+
+clearMaze();
